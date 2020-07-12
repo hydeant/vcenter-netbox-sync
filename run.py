e72502604cf9bbfe2d86e22b4a793045583288d4
Marellan <marellan@thelost.dev>
2020-07-12 16:29:32 -0700

    Update run.py

--- run.py
+++ run.py
@@ -711,6 +711,7 @@ class vCenterHandler:
                     results["virtual_machines"].append(nbt.virtual_machine(
                         name=truncate(obj_name, max_len=64),
                         cluster=cluster,
+                        comments=getattr(obj.config, "annotation", None),
                         status=int(
                             1 if obj.runtime.powerState == "poweredOn" else 0
                             ),
